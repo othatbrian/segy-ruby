64134b52eeef8b81502f502104179be902e0ae51
Brian Stevens <brian.stevens@dataporters.com>
2014-12-10 02:58:20 -0600

    Parse EBCDIC SEGY file header

--- lib/segy.rb
+++ lib/segy.rb
@@ -1,4 +1,7 @@
+require File.dirname(__FILE__) + '/ebcdic'
+
 class Segy
+
   def self.open(file)
     instance = allocate
     instance.instance_variable_set(:@file, File.open(file))
@@ -8,4 +11,9 @@ class Segy
   def close
     @file.close
   end
+
+  def file_header
+    @file.read(3200).unpack('C*').collect { |int| Ebcdic.to_ascii int }.join
+  end
+
 end
